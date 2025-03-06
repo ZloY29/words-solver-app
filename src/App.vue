6c1603f8e09f2959ce4829da03060be6d8ec3d57
ZloY29 <lysikovd@list.ru>
2025-03-06 14:00:08 +0300

    Added standalone mode for Apple phones, added app's avatar, and new name for app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app">
-    <h1>Photo Word Finder</h1>
+    <h1>Word Helper</h1>
     <input type="file" accept="image/*" @change="handleFileChange" />
     <button @click="uploadImage" :disabled="!selectedImage">Upload Image</button>
     <div v-if="loading">Loading...</div>
