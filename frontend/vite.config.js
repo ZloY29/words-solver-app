0babdbfa6f846337a7b6db404cbf0e035152347e
ZloY29 <lysikovd@list.ru>
2026-01-07 12:56:57 +0300

    fix vite proxy to backend 8000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/upload': {
-        target: 'http://localhost:5137', // Адрес вашего backend'а
-        changeOrigin: true,              // Разрешаем изменение заголовков
-        rewrite: (path) => path.replace(/^\/upload/, '/upload'), // Перенаправляем на /upload
-      },
+      "/upload": { target: "http://localhost:8000", changeOrigin: true },
+      "/add_word": { target: "http://localhost:8000", changeOrigin: true },
+      "/remove_words": { target: "http://localhost:8000", changeOrigin: true },
     },
   },
-})
+});
